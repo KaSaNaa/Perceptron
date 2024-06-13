d34358925e90cee2314a64acb6b4beaf15c325ea
KaSaNaa <pasindudeshitha123@gmail.com>
2024-06-14 04:22:16 +0530

    feat: Add load_labels and encode_fives functions to MNIST reader library

--- MNIST-reader-library/mnist.py
+++ MNIST-reader-library/mnist.py
@@ -8,7 +8,7 @@ def load_images(filename):
     with gzip.open(filename, 'rb') as f:
         # Read the header information into a bunch of variables
         _ignored, n_images, columns, rows = struct.unpack('>IIII', f.read(16))
-        # Read all tthe pixels into a NumPy array of bytes
+        # Read all the pixels into a NumPy array of bytes
         all_pixels = np.frombuffer(f.read(), dtype=np.uint8)
         # Reshape the pixels into  matrix where each line is an image:
         return all_pixels.reshape(n_images, columns * rows)
@@ -17,3 +17,17 @@ def prepend_bias(X):
     # Insert a column of 1s in the postion of X
     # ("axis=1" stands for: "inserrt a column, not a row")
     return np.insert(X, 0, 1, axis=1)
+
+def load_labels(filename):
+    # Open and unzip the file of images
+    with gzip.open(filename, 'rb') as f:
+        # Skip the header bytes:
+        f.read(8)
+        # Read all the labels into a list:
+        all_labels = f.read()
+        # Reshape the list of labels into a one-column matrix:
+        return np.frombuffer(all_labels, dtype=np.uint8).reshape(-1, 1)
+    
+def encode_fives(Y):
+    # Convert all 5s to 1, and everything else to 0
+    return (Y == 5).astype(int)
