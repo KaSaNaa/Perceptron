d1d9d6af5d06352084538fc4f35476dcd38d7c48
KaSaNaa <pasindudeshitha123@gmail.com>
2024-06-14 21:33:04 +0530

    feat: Add logistic regression implementation for MNIST digit recognition

---
+++ MNIST-reader-library/main.py
@@ -0,0 +1,51 @@
+# A binary classifier that recognizes one of the digits in MNIST.
+
+import numpy as np
+
+# Applying Logistic Regression
+def sigmoid(z):
+    return 1 / (1 + np.exp(-z))
+
+# Basically doing prediction but named forward as its 
+# performing Forward-Propagation
+def forward(X, w):
+    weighted_sum = np.matmul(X, w)
+    return sigmoid(weighted_sum)
+
+# Calling the predict() function
+def classify(X, w):
+    return np.round(forward(X, w))
+
+
+# Computing Loss over using logistic regression
+def loss(X, Y, w):
+    y_hat = forward(X, w)
+    first_term = Y * np.log(y_hat)
+    second_term = (1 - Y) * np.log(1 - y_hat)
+    return -np.average(first_term + second_term)
+
+
+# calculating gradient
+def gradient(X, Y, w):
+    return np.matmul(X.T, (forward(X, w) - Y)) / X.shape[0]
+
+# calling the training function for desired no. of iterations
+def train(X, Y, iterations, lr):
+    w = np.zeros((X.shape[1], 1))
+    for i in range(iterations):
+        print('Iteration %4d => Loss: %.20f' % (i, loss(X, Y, w)))
+        w -= gradient(X, Y, w) * lr
+    return w
+
+# Doing inference to test our model
+def test(X, Y, w):
+    total_examples = X.shape[0]
+    correct_results = np.sum(classify(X, w) == Y)
+    success_percent = correct_results * 100 / total_examples
+    print("\nSuccess: %d/%d (%.2f%%)" %
+          (correct_results, total_examples, success_percent))
+
+# Test it
+import mnist as data
+w = train(data.X_train, data.Y_train, iterations=100, lr=1e-5)
+test(data.X_test, data.Y_test, w)
